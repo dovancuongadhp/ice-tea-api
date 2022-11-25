4a5a1da1e0728c5f96f0d11a79c858be39cdf0f3
CuongKun <dovancuongadhp@gmail.com>
2022-11-26 02:45:53 +0700

    test-config-authorization

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,6 @@
 import * as mongoose from "mongoose";
 export default async function ConnectMongoDb() {
   mongoose.connect(process.env.MONGODB_URL, () => {
-    console.log("--> Connected to MongoDB");
+    console.log("--> Connected to MongoDB <--");
   });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
-import * as express from "express";
-import * as morgan from "morgan";
-import * as bodyParser from "body-parser";
-import AllRouter from "./routers/index";
-import * as cors from "cors";
-import * as fs from "fs";
-import * as path from "path";
-import * as dotenv from "dotenv";
-import ConnectMongoDb from "./config/database";
+import * as express from 'express';
+import * as morgan from 'morgan';
+import * as bodyParser from 'body-parser';
+import AllRouter from './routers/index';
+import * as cors from 'cors';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+import * as jwt from 'jsonwebtoken';
+import ConnectMongoDb from './config/database';
 const app = express();
 app.use(bodyParser.json());
 
 // [LOGGER]
+/** 
+ * Print Logger
 app.use(
   morgan("common", {
     stream: fs.createWriteStream(path.join(__dirname, "log/access.log"), {
@@ -19,8 +22,12 @@ app.use(
   })
 );
 
+*/
+
+// [LOGGER COMBINED]
+app.use(morgan('combined'));
 // [CORS]
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: '*' }));
 
 // [ROUTERS]
 AllRouter(app);
@@ -31,6 +38,44 @@ dotenv.config();
 // [CONNECT MONGODB]
 ConnectMongoDb();
 
+// JWT
+
+const productList = [
+  {
+    id: 1,
+    name: 'iphone 14',
+    price: '200$'
+  },
+  {
+    id: 2,
+    name: 'Macbook pro',
+    price: '300$'
+  }
+];
+
+app.post('/login', (req, res) => {
+  const data = req.body;
+  const accessToken = jwt.sign(data, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '100s' });
+  return res.json(accessToken);
+});
+
+app.get('/productList', authenToken, (req, res) => {
+  res.json({ status: 200, data: productList });
+});
+
+function authenToken(req, res, next) {
+  const authorizationHeader = req.headers['authorization'];
+  // Beaer [Token]
+  const token = authorizationHeader.split(' ')[1];
+  if (!token) res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
+    console.log(err, data);
+    if (err) res.sendStatus(403);
+    next();
+  });
+}
+
 app.listen(process.env.PORT, () => {
-  console.log("The application is listening on port 8000!");
+  console.log('The application is listening on port 8000!');
 });

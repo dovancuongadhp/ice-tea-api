7c404080442cf2c2e6343ab57320dfb77062a711
Do Cuong <cuongdkun@Dos-iMac.local>
2023-02-10 16:15:42 +0700

    unit test

--- test/product.js
+++ test/product.js
@@ -3,7 +3,8 @@ process.env.NODE_ENV = 'test';
 //Require the dev-dependencies
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let server = require('../server');
+let server = require('../dist/index');
+
 let should = chai.should();
 
 chai.use(chaiHttp);
@@ -21,10 +22,27 @@ describe('product',()=>{
                 .get('/api-products/getAllProducts')
                 .end((err, res) => {
                     res.should.have.status(200);
-                    res.body.should.be.a('array');
-                    res.body.length.should.be.eql(9); // fixme :)
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message').eql('oke');
+                    res.body.should.have.property('status').eql(200);
+                    res.body.data[0].should.have.property('price').eql("3000");
                     done();
                 });
         });
+        it('it should GET product by id', (done) => {
+            let id = '63e5ecede08955523fee736d';
+            chai.request(server)
+                .get('/api-products/getProductById/'+id)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message').eql('Successfully');
+                    res.body.should.have.property('status').eql(200);
+                    res.body.should.have.property('data').be.a('object');
+                    res.body.data.should.have.property('price').eql("3000");
+                    done();
+                });
+        });
+
     });
 })

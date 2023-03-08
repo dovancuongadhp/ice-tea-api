9d1417f8d429aa4765c98548af6f712cb07ceb19
CuongKun <dovancuongadhp@gmail.com>
2023-03-08 19:24:14 +0700

    config alias path

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import appRoutes from './routers';
 import appConfig from './config';
 import Banner from './banner'
-
+import UserService from "@src/services/UsersService"
 const app:express.Application = express();
 
 // [CONFIG]
@@ -18,6 +18,9 @@ app.get('/hello',(req,res)=>{
     res.send('Hello World on port 8000')
 })
 
+UserService.getAllUsers()
+
+console.log(process.env.TS_NODE_BASEURL)
 
 const port = process.env.PORT
 app.listen(port, () => {
